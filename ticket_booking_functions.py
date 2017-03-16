3ae1fae50113785679f94420cb57adbbaf2075f6
MissObange <lindy.sharon1@gmail.com>
2017-03-17 00:14:42 +0300

    Updated event_edit method

--- ticket_booking_functions.py
+++ ticket_booking_functions.py
@@ -18,39 +18,66 @@ session = DBSession()
 class New_events():
 
 
-	def add_events(self, eventName, startDate, endDate, eventVenue):
+	def event_create(self, eventName, startDate, endDate, eventVenue):
 
 
 		events = Event_details(event_name = eventName, start_date= startDate, end_date= endDate, venue = eventVenue)
 		session.add(events)
 		session.commit()
+		return "Successfully added"
 		
 
-	def view_events_list():
-		view_events_list = session.query(Event_details).all()
+	def event_list():
+		event_list = session.query(Event_details).all()
 		list_events = []
 
-		for item in view_events_list:
+		for item in event_list:
 			list_item = [item.event_id, item.event_name, item.start_date, item.end_date, item.venue]
 			list_events.append(list_item)
-			
 
 			if len(list_events)==0:
-				print ("There are no upcoming events currently stored in the database")
-		
-		return list_events			
+				return ("There are no upcoming events currently stored in the database")
 
-	def delete_event(eventID):
-		
+			
+		return list_events
+
+	def event_delete(eventID):
 		to_delete = session.query(Event_details).filter_by(event_id = eventID).first()
 		session.delete(to_delete)
 		session.commit()
+		return('Successfully deleted!')
+
+	def event_edit(event_ID):
+		to_edit = session.query(Event_details).filter_by(event_id = event_ID).first()
+		new_name = input("Enter new name to change from '%s' (Enter '0' to leave as is):  "%(to_edit.event_name))
+		if new_name != 0:
+			to_edit.event_name = new_name
+		else: 
+			pass
+
+		new_start_date = input(("Enter new date to change from '%s' (Enter '0' to leave as is):  "%(to_edit.start_date)))
+		if new_start_date !='0':
+			to_edit.start_date = new_start_date
+
+		new_end_date = input(("Enter new date to change from '%s' (Enter '0' to leave as is):  "%(to_edit.end_date)))
+		if new_end_date != 0:
+			to_edit.end_date = new_end_date
+
+		new_venue = input(("Enter new date to change from '%s' (Enter '0' to leave as is):  "%(to_edit.venue)))
+		if new_venue != 0:
+			to_edit.venue = new_venue
+
+		session.add(to_edit)
+		session.commit()
+		return "Successfully edited"
+
+New_events()
 
 
 	
 
 			
-New_events()
+
 
 
 
