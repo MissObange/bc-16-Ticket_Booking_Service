688430af949b8d57a18401e8671e8feb45af9b8f
MissObange <lindy.sharon1@gmail.com>
2017-03-16 18:45:48 +0300

    Updated database setup(PEP 8 specifications)

--- ticket_booking_db_setup.py
+++ ticket_booking_db_setup.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine, ForeignKey
-
 from sqlalchemy import Column, Date, Integer, String, VARCHAR
-
 from sqlalchemy.orm import relationship
-
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///ticket_booking_database.db')
+engine = create_engine('sqlite:///ticket_booking_DB.db')
+print (engine)
 
 Base = declarative_base()
 
@@ -15,15 +13,10 @@ Base = declarative_base()
 class Event_details(Base):
 
     __tablename__ = "events"
-
     event_id = Column(Integer, primary_key=True)
-
     event_name = Column(VARCHAR(50))
-
     start_date = Column(VARCHAR(9))
-
     end_date = Column(VARCHAR(9))
-
     venue = Column(VARCHAR(50))
 
 
@@ -32,15 +25,10 @@ class Event_details(Base):
 
 class Ticket_details(Base):
 	__tablename__ = "tickets"
-
-
 	ticket_id = Column(Integer, primary_key = True)
-
 	event_id = Column(Integer, ForeignKey("events.event_id"))
 	event = relationship('Event_details', backref= 'events')
-	
 	customer_name = Column(String(50))
-
 	customer_email = Column(VARCHAR(50))
 
 
