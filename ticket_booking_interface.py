4013dfa3d157188ff9227f32d40b0ec70373aeef
MissObange <lindy.sharon1@gmail.com>
2017-03-16 15:48:24 +0300

    Fixed usage display error

--- ticket_booking_interface.py
+++ ticket_booking_interface.py
@@ -1,16 +1,15 @@
+#!venv/bin/python
 """
-
+This example uses docopt with the built in cmd module to demonstrate an
+interactive command application.
 Usage:
-    event add_event <event_name> ...
-    event view_events
-    event delete_event <event_ id>
-    event edit_event <event_id>
-
-    ticket view_tickets <event_id>
-
-    ticket (-i | --interactive)
-    ticket (-h | --help)
-
+    my_app add_event <event_name>
+    my_app list_events
+    my_app delete_event <event_id>
+    my_app edit_event <event_id>
+    my_app view_tickets <event_id>
+    my_app (-i | --interactive)
+    my_app (-h | --help)
 Options:
     -i, --interactive  Interactive Mode
     -h, --help  Show this screen and exit.
@@ -19,7 +18,8 @@ Options:
 import sys
 import cmd
 from docopt import docopt, DocoptExit
-from ticket_booking_functions import new_events
+from ticket_booking_functions import New_events
+
 
 
 def docopt_cmd(func):
@@ -27,9 +27,9 @@ def docopt_cmd(func):
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
     """
-    def fn(self, args):
+    def fn(self, arg):
         try:
-            opt = docopt(fn.__doc__, args)
+            opt = docopt(fn.__doc__, arg)
 
         except DocoptExit as e:
             # The DocoptExit is thrown when the args do not match.
@@ -53,57 +53,66 @@ def docopt_cmd(func):
     return fn
 
 
-class event_actions(cmd.Cmd):
-    intro = 'Ticket Booking Service Application'
-    prompt = 'event>> '
+class Event_actions(cmd.Cmd):
+    intro = 'Welcome to Ticket Booking Service System'
+    prompt = 'Enter action>> '
     file = None
 
-    
     @docopt_cmd
-    def do_add_event(self, args):
+    def do_add_event(self, arg):
+        """Usage:
+                add_event <event_name>
         """
-        Usage:
-        	add_event <event_name> ... 
-        """
-        eventName= args["<event_name>"]
+        eventName= arg["<event_name>"]
         startDate= input("Enter the event start date in the format mm/dd/yy: ")
         endDate= input("Enter the event end date in the format mm/dd/yy: ")
         eventVenue = input("enter the event venue:")
-        new_events.add_events(eventName, startDate, endDate, eventVenue)
 
+        add_new_event = New_events()
+        print (add_new_event.add_events(eventName, startDate, endDate, eventVenue))
 
     @docopt_cmd
-    def do_view_events(self, args):
-         """
-        Usage:
-            <view_events> 
+    def do_list_events(self, arg):
+        """Usage:
+                list_events
         """
-        new_events.view_events_list()
-                                       
-    @docopt_cmd
-    def do_delete_event(self,args):
+        print(subtraction(arg['<numberA>'], arg['<numberB>']))
 
-         """
-        Usage:
-            delete_event <event_id> 
+    @docopt_cmd
+    def do_delete_event(self, arg):
+        """Usage: 
+                delete_event <event_id>
         """
-        selected_event = args["<event_id>"]
-        new_events.delete_event(selected_event)
+        print(division(arg['<numberA>'], arg['<numberB>']))
 
-
-    
     @docopt_cmd
-    def do_quit(self, args):
+    def do_edit_event(self, arg):
+        """Usage: 
+            edit_event <event_id> 
+        """
+        print(multiplication(arg['<numberA>'], arg['<numberB>']))
+
+    @docopt_cmd
+    def do_view_tickets(self,arg):
+        """Usage: 
+                view_tickets <event_id>
+        """
+        print(multiplication(arg['<numberA>'], arg['<numberB>']))
+
+
+
+    def do_quit(self, arg):
         """Quits out of Interactive Mode."""
+
         print('Good Bye!')
         exit()
 
 
-option = docopt(__doc__, sys.argv[1:])
+opt = docopt(__doc__, sys.argv[1:])
 
-if option['--interactive']:
+if opt['--interactive']:
     try:
         print(__doc__)
-        event_actions().cmdloop()
+        Event_actions().cmdloop()
     except KeyboardInterrupt:
-    	print("Exiting App")
+        print("Exiting App")
